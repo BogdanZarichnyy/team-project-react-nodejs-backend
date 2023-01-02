b776998d3643354596b32672d0ba247fb16db5d5
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2023-01-02 14:40:13 +0200

    5

--- helpers/petsMessages.js
+++ helpers/petsMessages.js
@@ -1,7 +1,7 @@
 const NOT_VALID_ID =
   "ID is not valid for MongoDB documents, please enter correct 'petId'";
 const MISSING_FIELD = field => `Missing field: { "${field}" }`;
-const NOT_FOUND_PET_FOR_UPDATE = 'Not fount pet for update';
+const NOT_FOUND_PET_FOR_UPDATE = 'Not found pet for update';
 const FOUNDED_DATA = 'All results on your request';
 const NOT_FOUND_PET = 'Not found pet on your request';
 const DELETE_PET_SUCCESS = 'Pet deleted successfully';
