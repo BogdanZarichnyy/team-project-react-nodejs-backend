1211629d92caff72c9a0beaf45633c5c79e2fdb1
Oleg Palashynskyi <106759871+0Palah@users.noreply.github.com>
2023-01-03 20:19:53 +0200

    forgotPassword

--- controllers/users/forgotUserPassword.js
+++ controllers/users/forgotUserPassword.js
@@ -1,22 +1,45 @@
 const bcrypt = require('bcryptjs');
 const { createError } = require('../../helpers/createError');
 const User = require('../../models/userModel');
-require('dotenv').config();
+const { randomUUID } = require('crypto');
+const sendSgEmail = require('../../helpers/sendSgEmail');
+
+// require('dotenv').config();
+
+const { BASE_URL } = process.env;
 
 const forgotUserPassword = async (req, res) => {
-    const { email, password } = req.body;
+  const { email } = req.body;
+  const random = randomUUID();
+  const password = random.slice(0, 8);
+  console.log(random);
+  console.log(password);
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        throw createError({ status: 409, message: `No user with e-mail ${email} found` });
-    }
+  if (!user) {
+    throw createError({
+      status: 409,
+      message: `No user with e-mail ${email} found`,
+    });
+  }
 
-    const bcryptHashPassword = await bcrypt.hash(password, 10);
+  const bcryptHashPassword = await bcrypt.hash(password, 10);
 
-    await User.findByIdAndUpdate(user._id, { password: bcryptHashPassword });
+  const message = {
+    to: email,
+    subject: 'Forgot password',
+    text: `<Here is your temporary password:${password}`,
+    html: `<p>Here is your temporary password:<b>${password}</b></p>`,
+  };
 
-    res.status(200).json({ message: `Created new password for user ${user.name}` });
-}
+  await sendSgEmail(message);
+
+  await User.findByIdAndUpdate(user._id, { password: bcryptHashPassword });
+
+  res
+    .status(200)
+    .json({ message: `Created new password for user ${user.name}` });
+};
 
 module.exports = forgotUserPassword;
