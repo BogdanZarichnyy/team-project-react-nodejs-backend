c0d92094f67a61f5f6e5735d510c4d1c613da6b5
BogdanZarichnyy <107411214+BogdanZarichnyy@users.noreply.github.com>
2023-01-10 22:12:13 +0200

    Update petValidation.js

--- validation/petValidation.js
+++ validation/petValidation.js
@@ -2,13 +2,30 @@ const Joi = require('joi');
 const regExp = require('../helpers/regExp');
 
 const addMyPetValidationSchema = Joi.object({
-  name: Joi.string().pattern(regExp.nameRegExp).min(2).max(16).required(),
-  family: Joi.string().pattern(regExp.stringRegExp).allow('').optional(),
-  birthDate: Joi.date().required(),
-  breed: Joi.string().pattern(regExp.stringRegExp).min(2).max(24).required(),
-  sex: Joi.string().optional(),
-  photo: Joi.string().allow('').optional(),
-  passport: Joi.string().allow('').optional(),
+  name: Joi.string()
+    .pattern(regExp.nameRegExp)
+    .min(2)
+    .max(16)
+    .required(),
+  family: Joi.string()
+    .pattern(regExp.stringRegExp)
+    .allow('')
+    .optional(),
+  birthDate: Joi.date()
+    .required(),
+  breed: Joi.string()
+    .pattern(regExp.stringRegExp)
+    .min(2)
+    .max(24)
+    .required(),
+  sex: Joi.string()
+    .optional(),
+  photo: Joi.string()
+    .allow('')
+    .optional(),
+  passport: Joi.string()
+    .allow('')
+    .optional(),
   comments: Joi.string()
     .pattern(regExp.lettersAndDigitsRegExp)
     .min(8)
@@ -18,13 +35,30 @@ const addMyPetValidationSchema = Joi.object({
 });
 
 const updateMyPetValidationSchema = Joi.object({
-  name: Joi.string().pattern(regExp.nameRegExp).min(2).max(16).optional(),
-  family: Joi.string().pattern(regExp.stringRegExp).allow('').optional(),
-  birthDate: Joi.date().optional(),
-  breed: Joi.string().pattern(regExp.stringRegExp).min(2).max(24).optional(),
-  sex: Joi.string().optional(),
-  photo: Joi.string().allow('').optional(),
-  passport: Joi.string().allow('').optional(),
+  name: Joi.string()
+    .pattern(regExp.nameRegExp)
+    .min(2)
+    .max(16)
+    .optional(),
+  family: Joi.string()
+    .pattern(regExp.stringRegExp)
+    .allow('')
+    .optional(),
+  birthDate: Joi.date()
+    .optional(),
+  breed: Joi.string()
+    .pattern(regExp.stringRegExp)
+    .min(2)
+    .max(24)
+    .optional(),
+  sex: Joi.string()
+    .optional(),
+  photo: Joi.string()
+    .allow('')
+    .optional(),
+  passport: Joi.string()
+    .allow('')
+    .optional(),
   comments: Joi.string()
     .pattern(regExp.lettersAndDigitsRegExp)
     .min(8)
