fd7163dbd5e299f4013db4cf5aa9a55b61deda17
BogdanZarichnyy <bogdanzarichnyy@gmail.com>
2023-01-11 20:42:49 +0200

    fix

--- controllers/ads/getMyAds.js
+++ controllers/ads/getMyAds.js
@@ -2,7 +2,7 @@ const Ad = require('../../models/adModel');
 
 const getMyAds = async (req, res) => {
     const { _id } = req.user;
-    const { page = 1, limit = 16, query = '' } = req.query;
+    const { page = 1, limit = 8, query = '' } = req.query;
     const skip = (parseInt(page) - 1) * parseInt(limit);
 
     const data = await Ad.find({ owner: _id, addTitle: { $regex: new RegExp(query, 'i') } })
